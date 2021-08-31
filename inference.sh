1a7a000b24151985413eb19ba0934bf9e09d90a8
Moliner Eloi <molinee2@login3.triton.aalto.fi>
2021-08-31 15:41:56 +0300

    preparing files

--- inference.sh
+++ inference.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#module load anaconda
-
-#source activate /scratch/work/molinee2/conda_envs/unet_env
-
 
 python inference.py path_experiment="experiments/trained_model" inference.audio=$1
 
--- train.sh
+++ train.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-#module load anaconda
-#module load matlab/r2020b
-#
-#source activate /scratch/work/molinee2/conda_envs/unet_env
-
 n=1
 PATH_EXPERIMENT=/scratch/work/molinee2/unet_dir/unet_denoising_github/experiments/${n}
 mkdir $PATH_EXPERIMENT
 
-python  train.py path_experiment="$PATH_EXPERIMENT"  epochs=150 freq_inference=50 batch_size=2 steps_per_epoch=2000
+
+python  train.py path_experiment="$PATH_EXPERIMENT"  epochs=150  batch_size=2 steps_per_epoch=2000
