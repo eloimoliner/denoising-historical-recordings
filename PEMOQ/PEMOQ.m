0fe16d77be8ff4f2c8e68cf3f28cf0d98ae55460
Moliner Eloi <molinee2@login3.triton.aalto.fi>
2021-08-31 11:16:16 +0300

    removing test

--- PEMOQ/PEMOQ.m
+++
@@ -1,5 +0,0 @@
-function [ODG]=PEMOQ(ref,test)
-[refa, fs]=audioread(ref);
-[testa, fs]=audioread(test);
-[PSM, PSMt, ODG, PSM_inst] = audioqual(refa, testa, fs);
-
--- PQevalAudio/PEAQTest.m
+++
@@ -1,9 +0,0 @@
-clear; clc; 
-%addpath('PQevalAudio', 'PQevalAudio/CB','PQevalAudio/Misc','PQevalAudio/MOV', 'PQevalAudio/Patt')  
-
-ref = '2_clean_48.wav' % 16 times oversampling
-test = '2_noise_48.wav' % 4 times oversampling
-%test = '../2_output_48.wav' % 4 times oversampling
-
-
-[odg, movb] = PQevalAudio(ref, test)
